7f040958f138a5f0176efb0b10d7403b1afd4d83
Bijal Patel <bhopkins@q-centrix.com>
2019-10-12 15:09:36 -0700

    Make 2nd modal on the same page work

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -5,7 +5,8 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
+var span = document.getElementById("modalClose");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -21,5 +22,31 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementById("modalClose2");
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
